b68b1cb4e16180b1cea9150d8156769b5e234da8
keisuke kudo <melchior.balthasar.casper326@gmail.com>
2016-08-12 19:55:55 +0900

    Program.cs deleting redundant using

--- Challenge3/Program.cs
+++ Challenge3/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Challenge3.Model;
 using Newtonsoft.Json;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Challenge3 {
     class Program {
