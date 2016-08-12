fece3d7d52984b79d7eb394b1e67d2655de36c54
keisuke kudo <melchior.balthasar.casper326@gmail.com>
2016-08-12 19:32:36 +0900

    add JsonFilePath class

--- Challenge3/Program.cs
+++ Challenge3/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Challenge3 {
     class Program {
+
+        #region テスト用JSONファイル名定義クラス
+
+        private static class JsonFilePath {
+            //JSONファイルディレクトリ
+            private const string JsonDirectry = "./Json/";
+
+            //各テストファイル
+            public static readonly string Sample141 = $"{JsonDirectry}sample_data_141.json";
+        }
+
+        #endregion
+
         static void Main(string[] args) {
         }
     }
